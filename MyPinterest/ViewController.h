3fe011d915ab077354a30ff4506ffec6d4077a40
Felicia Weathers <Fel.Weathers@gmail.com>
2017-02-26 13:54:11 -0500

    first commit

---
+++ MyPinterest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPinterest
+//
+//  Created by Felicia Weathers on 2/26/17.
+//  Copyright © 2017 Felicia Weathers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyPinterest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyPinterest
+//
+//  Created by Felicia Weathers on 2/26/17.
+//  Copyright © 2017 Felicia Weathers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
